9fbd76e1e79cd1c398fa3b569f4eeb31eca07814
ZSM <maynard.zach23@gmail.com>
2023-02-24 14:51:08 -0500

    homepage

--- src/components/Splash/Announcements/Announcements.js
+++ src/components/Splash/Announcements/Announcements.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Announcements.scss';
 import KC_Store from '../../../assets/web/kanawha_location.jpg'
+import CL_Store from '../../../assets/web/crosslanes_location.jpg'
 
 import { Link } from "react-router-dom";
 
@@ -10,16 +11,19 @@ const { Meta } = Card;
 const Announcements = (props) => {
   return(
     <div>
+      <div className='announcement_container'>
+        <h1 style={{ textAlign: 'center', fontFamily: 'DomainLight', color: '#425030'}}>We're pleased to announce the grand opening of our new location in Kanawha City!</h1>
+      </div>
       <div className='card_container'>
         <Card
           hoverable
           style={{
-            minWidth: '350px',
-            maxWidth: '768px'
+           width: 350,
+           margin: '1em'
           }}
           cover={<img alt="example" src={KC_Store} style={{ height: '100%', width: '100%' }} />}
           >
-          <Meta title="Kanawha City is now open!" description="We are pleased to announce the grand opening of Greene Street Cannabis Co. at 5126 MacCorkle Ave SE, Charleston, WV 25304" />
+          <Meta title="Shop Kanawha City" description="5126 MacCorkle Ave SE, Charleston, WV 25304" />
           <div className='card_button_container'>
             <Button>
               <Link to='/shop/wv/kanawha'>
@@ -28,18 +32,24 @@ const Announcements = (props) => {
             </Button>
           </div>
         </Card>
-        <div className='button_container'>
-          <Button>
-            <Link to='/shop/wv/crosslanes'>
-              Shop Cross Lanes
-            </Link>
-          </Button>
-          <Button>
-            <Link to='/shop/wv/kanawha'>
-              Shop Kanawha
-            </Link>
-          </Button>
-        </div>
+        
+        <Card
+          hoverable
+          style={{
+            width: 350,
+            margin: '1em'
+          }}
+          cover={<img alt="example" src={CL_Store} style={{ height: '100%', width: '100%' }} />}
+          >
+          <Meta title="Shop Cross Lanes" description="5126 MacCorkle Ave SE, Charleston, WV 25304" />
+          <div className='card_button_container'>
+            <Button>
+              <Link to='/shop/wv/crosslanes'>
+                Browse menu
+              </Link>
+            </Button>
+          </div>
+        </Card>
       </div>
       {/* <div className="parallax"></div>
       <div style={{ textAlign: 'center', fontFamily: 'DomainLight' }}>
